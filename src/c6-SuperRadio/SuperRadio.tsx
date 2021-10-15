8b85611925938e24d1929da6cfec8ca52235c885
KattyLearner <ekaterina.derzhitskaya@gmail.com>
2021-10-15 17:36:27 +0300

    first commit

---
+++ src/c6-SuperRadio/SuperRadio.tsx
@@ -0,0 +1,50 @@
+import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
+import s from './SuperRadio.module.css'
+
+type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+type SuperRadioPropsType = DefaultRadioPropsType & {
+    options?: any[]
+    onChangeOption?: (option: any) => void
+}
+
+const SuperRadio: React.FC<SuperRadioPropsType> = (
+    {
+        type, name,
+        options, value,
+        onChange, onChangeOption,
+        ...restProps
+    }
+) => {
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e)
+        onChangeOption && onChangeOption (e.currentTarget.value)
+        // onChange, onChangeOption
+    }
+
+    const finalClassNameRadio = s.radio
+
+    const mappedOptions: any[] = options ? options.map((o, i) => (
+        <label key={name + '-' + i}>
+            <input
+                type={'radio'}
+                className={finalClassNameRadio}
+                name={name}
+                checked={o === value}
+                value={o}
+                onChange={onChangeCallback}
+                {...restProps}
+                // name, checked, value, onChange
+            />
+            {o}
+        </label>
+    )) : []
+
+    return (
+        <>
+            {mappedOptions}
+        </>
+    )
+}
+
+export default SuperRadio
